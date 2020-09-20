efcccea80e0dc663b6ce2169d7adf948f907ceb8
BykTupogub <BykTupogub@users.noreply.github.com>
2020-09-20 17:15:20 +0300

    последний номер

---
+++ homework_1_9/homework_1_9.cpp
@@ -0,0 +1,17 @@
+﻿// homework_1_9.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, char* argv[])
+{
+    setlocale(LC_ALL, "Russian");
+    int a = 0, n = 0;
+    cout << "введите степень" << endl;
+    cin >> n;
+    a = 1 << n;
+    a = ~(-a);
+    cout << "2^" << n << " - 1 =  " << a << endl;
+    return EXIT_SUCCESS;
+}
+
