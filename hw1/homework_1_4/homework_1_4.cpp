5f4fc8028e48b203aa7db1c75a47d1fec5312796
BykTupogub <BykTupogub@users.noreply.github.com>
2020-09-23 19:51:44 +0300

    закинул в одну папку и исправил пятое

---
+++ hw1/homework_1_4/homework_1_4.cpp
@@ -0,0 +1,33 @@
+﻿// homework_1_4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "Russian");
+	int a = 0;
+	bool t = true;
+	cout << "введите число:";
+	cin >> a;
+	cout << endl;
+	for (int i = a; i > 0; i--)
+	{
+		for (int j = i - 1; j > 1; j--)
+		{
+			if (i % j == 0)
+			{
+				t = false;
+			}
+			
+		}
+		if (t == true)
+		{
+			cout << i << " ";
+		}
+		t = true;
+	}
+	cout << endl;
+	return EXIT_SUCCESS;
+}
---
+++ hw1/homework_1_6/homework_1_6.cpp
@@ -0,0 +1,18 @@
+﻿// homework_1_6.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, char* argv[])
+{
+    setlocale(LC_ALL, "Russian");
+    int a = 0, b = 1;
+    cout << "введите число" << endl;
+    cin >> a;
+    for (int i = 1; i <= a; i++)
+    {
+        b *= i;
+    }
+    cout << "факториал числа a = " << b << endl;
+    return EXIT_SUCCESS;
+}
